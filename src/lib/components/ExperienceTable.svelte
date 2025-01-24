2b3b7bc6bc5c3ade9b9568a48913dab71b7c6769
cepoumian <cepoumian@gmail.com>
2025-01-24 11:27:30 -0600

    Create Hero and About Me sections

---
+++ src/lib/components/ExperienceTable.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+  interface WorkExperience {
+    jobTitle: string;
+    company: string;
+    startDate: string;
+    endDate?: string;
+  }
+  
+  const workExperience: WorkExperience[] = [
+  {
+    jobTitle: "Software Engineer",
+    company: "cleverklagen",
+    startDate: "2021-03",
+  },
+  {
+    jobTitle: "Data Scientist",
+    company: "FootballRadar",
+    startDate: "2019-08",
+    endDate: "2020-11",
+  },
+  {
+    jobTitle: "Data Scientist",
+    company: "DAZN",
+    startDate: "2018-11",
+    endDate: "2019-07",
+  },
+  {
+    jobTitle: "Data Scientist - Master Thesis",
+    company: "FC Barcelona",
+    startDate: "2018-02",
+    endDate: "2018-08",
+  },
+];
+
+</script>
+
+<section class="default-margin work-experice mt-m">
+  <ul class="work-experice-list"></ul>
+  <h2 class="headline">Past work <br/> Experience</h2>
+</section>
+
+<style>
+  .work-experience {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  .work-experience-list {
+    width: 50%;
+  }
+
+  .headline {
+    text-align: right;
+  }
+
+  .work-item {
+    border-bottom: 1px solid #f0eded;
+    padding-bottom: 12px;
+  }
+
+  .work-item:not(:first-of-type) {
+    padding-top: 16px;
+  }
+
+  .work-item p {
+    margin-bottom: 0;
+  }
+
+  .company-and-date {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
---
+++ src/lib/components/Sections/HeroSection.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import Button from "$components/Button.svelte";
+
+  function onclick() {
+    goto("/#contact-form");
+  }
+</script>
+<section class="hero-section default-margin">
+  <h3>Hello! I'm Cesar</h3>
+  <div class="underscore mb-m"></div>
+  <h1>I'm a frontend developer crafting intuitive and performant</h1>
+  <h1 class="dark-grey mb-m">web experiences</h1>
+  <Button {onclick}>Let's talk</Button>
+</section>
+
+<style>
+  .hero-section {
+    padding-top: 60px;
+  }
+</style>
